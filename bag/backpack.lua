85341404f895448229a36fda795f8a8ee1ee1478
Antonio Lobato <aj.lobato@gmail.com>
2025-07-17 08:01:32 -0700

    WIP backpack

--- bag/backpack.lua
+++ bag/backpack.lua
@@ -1,19 +1,27 @@
 local moonlight = GetMoonlight()
-local context = moonlight:GetContext()
 
 --- Describe in a comment what this module does. Note the lower case starting letter -- this denotes a module package accessor.
 ---@class backpack
 local backpack = moonlight:NewClass("backpack")
 
 ---@class (exact) Backpack: Bag
+---@field container Container
+---@field sectionSet Sectionset
 local Backpack = {}
 
 --- Boot creates the backpack bag.
 function backpack:Boot()
   local window = moonlight:GetWindow()
   local engine = moonlight:GetSonataEngine()
-  Backpack.window = window:New()
+  local container = moonlight:GetContainer()
+  local sectionSet = moonlight:GetSectionset()
 
+  Backpack.window = window:New()
+  Backpack.container = container:New()
+  Backpack.sectionSet = sectionSet:New()
+  Backpack.container:SetChild(Backpack.sectionSet)
+
+  
   engine:RegisterBag(Backpack)
 end
 
