62b843ab8ea5f0d2086c91a636a9fc924899f5eb
Antonio Lobato <aj.lobato@gmail.com>
2025-07-30 00:04:27 -0700

    added popup types

--- popup/types.lua
+++ popup/types.lua
@@ -1,1 +1,13 @@
 ---@meta
+
+---@class PopupElement
+---@field Type "divider" | "label" | "item"
+---@field Title string
+---@field OnClick fun()
+---@field IsChecked fun():boolean
+---@field CloseOnClick boolean
+---@field SubMenu PopupElement[]
+
+---@class PopupConfig
+---@field Title string
+---@field Elements PopupElement[]
