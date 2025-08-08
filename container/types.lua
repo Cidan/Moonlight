f6d9fe253f29235a0b5f28eef2aaf398d945bcf4
Antonio Lobato <aj.lobato@gmail.com>
2025-08-07 19:29:27 -0700

    wip list config spec

--- container/types.lua
+++ container/types.lua
@@ -13,4 +13,21 @@
 ---@field Icon fileID | string
 ---@field Title? string
 
+--- ListRowContainer is the drawable unit for the row itself.
+--- It contains a single frame to which all items in the row must
+--- parent to. The ListRowContainer will render it's children as
+--- it comes into view.
+---@class ListRowContainer: Drawable
+---@field children Drawable
+
+--- ListColumn is a single column definition for a list.
+---@class ListColumn
+---@field Name string
+
+---@class ListRow
+---@field RowData table
+
 ---@class ListConfig
+---@field Columns ListColumn[] An ordered list of columns, from left to right.
+---@field RowRenderer fun(row: ListRow): Drawable
+---@field RowReleaser fun(row: ListRow, d: Drawable)
