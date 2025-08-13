5179bffe37c5ea2ad7236a618eaf2422e49f7595
Antonio Lobato <aj.lobato@gmail.com>
2025-08-13 15:00:36 -0700

    popup wip

--- popup/types.lua
+++ popup/types.lua
@@ -1,13 +1,13 @@
 ---@meta
 
 ---@class PopupElement
----@field Type "divider" | "label" | "item"
----@field Title string
----@field OnClick fun()?
----@field CanToggle boolean
----@field IsChecked? fun():boolean
----@field CloseOnClick boolean
----@field SubMenu? PopupElement[]
+---@field Type "divider" | "label" | "item" Type is the type of element this is in the popup.
+---@field Title string Title is the title of this item or label.
+---@field OnClick fun()? On click is an optional function that is called when this item is clicked. Only valid for item types.
+---@field CanToggle boolean CanToggle denotes that this item has an on and off state denoted by a checkbox drawn next to the item on the left.
+---@field IsChecked? fun():boolean IsChecked is a function that should be called when rendering a CanToggle box, that returns the state of the checkbox.
+---@field CloseOnClick boolean CloseOnClick tells the popup window to close when this item is clicked.
+---@field SubMenu? PopupElement[] SubMenu is a list of optional sub elements for creating nested popup windows.
 
 ---@class PopupConfig
 ---@field Title string
